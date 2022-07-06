446b54ecc5ad96cb56ffa5ff12075d84e50ac37c
Ari Nordström <ari.nordstrom@gmail.com>
2022-07-06 08:32:26 +0200

    Let script find out the repo path

--- sh/xslt-pipelines-3.sh
+++ sh/xslt-pipelines-3.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-#! /bin/sh -
 PROGNAME=$0
+ROOT=`cd $(dirname $(realpath -s $0))/..; pwd`
 
 usage() {
   cat << EOF >&2
-Usage: $PROGNAME [-r <root>] [-i <input>] [-t <output>]
+Usage: $PROGNAME [-i <input>] [-t <output>]
 
--r <root>: repository root
 -i <input>: input path
--t <output>: output path
+-t <output>: output base path
 EOF
   exit 1
 }
 
-root=default_root input=default_in output=default_out
-while getopts r:i:t: o; do
+input=default_in output=default_out
+while getopts i:t: o; do
   case $o in
-    (r) root=$OPTARG;;
     (i) input=$OPTARG;;
     (t) output=$OPTARG;;
     (*) usage
@@ -30,23 +28,6 @@ echo Remaining arguments: "$@"
 echo "Running" $PROGNAME
 echo "Input path" $input
 echo "Output base path" $output
-
-#PROJECT=`cd $1; pwd`
-# XSLT_MANIFEST=$1 # Path to XSLT manifest XML
-#SCH=$2 # Schematron for output
-#$SOURCES=$3 # Path to sources
-#TMP=$2 # Output base
-#$PUBLIC_ID=$5
-#$SYSTEM_ID=$6
-#$XSPEC_MANIFEST=$7 # XSpec manifest file
-#VERBOSE=$8 # Verbose output? true/false
-#DEBUG=$9 # Output debug? true/false
-#DTD_VALIDATE_INPUT=$10 # Validate input
-#DTD_VALIDATE_OUTPUT=$11 # Validate output
-#SCH_VALIDATE_OUTPUT=$12 # Validate output with Schematron
-#RUN_XSPECS=$13 # Run XSpecs - leave to false now!
-
-#ROOT=`cd $(dirname $(realpath -s $0))/..; pwd`
 
 MORGANA_HOME=/home/ari/MorganaXProc-IIIse-0.9.16-beta
 MORGANA_LIB=$MORGANA_HOME/MorganaXProc-IIIse_lib/*
@@ -68,10 +49,10 @@ java \
 $JAVA_AGENT \
 -cp $CLASSPATH com.xml_project.morganaxproc3.XProcEngine \
 -config=$MORGANA_HOME/config.xml \
-$root/../xproc-batch/xproc/validate-convert.xpl \
--catalogs=$root/../xproc-batch/catalogs/catalog.xml \
+$ROOT/../xproc-batch/xproc/validate-convert.xpl \
+-catalogs=$ROOT/../xproc-batch/catalogs/catalog.xml \
 -input:manifest=/home/ari/Documents/repos/xslt-pipelines/pipelines/test-manifest.xml \
--input:sch=$root/sch/placeholder.sch \
+-input:sch=$ROOT/sch/placeholder.sch \
 -option:input-base-uri=$input \
 -option:output-base-uri=$output/out \
 -option:reports-dir=$output/reports \
